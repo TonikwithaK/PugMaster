4d6d55454c346d6d9444d5c4ceb256e94ec06d9b
AnthonyReyes <reyes.anthony1437@gmail.com>
2025-01-13 18:12:41 -0500

    Flush Embeds for special cases and sort by recent.

--- commands/warns.ts
+++ commands/warns.ts
@@ -11,6 +11,9 @@ const command: Command = {
     category: 'info',
     shortDesc: 'List warned players',
     desc: 'List warned players',
+    args: [
+        { name: '[dump]', desc: 'Pass dump as argument to display dump all warns.', required: false }
+    ],
     defaults: [
         {
             type: 'string', name: 'show_issuer', desc: 'Display issuers as well in warns',
@@ -30,16 +33,35 @@ const command: Command = {
         const times = [];
         const reasons = [];
 
+        // add dump param to flush all warns
+        let dump = false;
+
+        if (params.length > 0) {
+            if (params[0].toLowerCase() !== 'dump') {
+                return await Util.send(message ? message : interaction, 'error', 'did you mean dump?');
+            }
+            else {
+                dump = true;
+            }
+        }
+
         const warns = await GuildModel.getWarns(BigInt(guild.id), 11);
 
         if (!warns.length) {
             return Util.send(message ? message : interaction, 'info', 'no warns found');
         }
 
-        warns.forEach((warn, index) => {
-            if (index === 10) {
-                return;
+        warns.sort((a, b) => {
+            if (a.warned_at > b.warned_at) {
+                return -1;
+            } else if (a.warned_at < b.warned_at) {
+                return 1;
+            } else {
+                return 0;
             }
+        });
+
+        warns.forEach((warn) => {
 
             const timeDif = (warn.warned_at.getTime() + guildSettings.warnExpiration) - new Date().getTime();
             const issuer = warn.issuer;
@@ -47,7 +69,7 @@ const command: Command = {
             let reason = warn.reason;
 
             if (reason && reason.length > 45) {
-                reason = reason.substr(0, 45) + '...';
+                reason = reason.substring(0, 45) + '...';
             }
 
             warnedPlayers.push(`${warn.warnid} - ${Util.removeMarkdown(warn.player)}${displayIssuers ? '\n' : ''}`);
@@ -56,18 +78,18 @@ const command: Command = {
             reasons.push(reason ? reason : '-');
         });
 
-        let fieldData;
+        let fieldDataComplete = [];
 
         if (displayIssuers) {
             const issuerReasonColumn = issuers.map((issuer, idx) => `${issuer}\n${reasons[idx]}`);
 
-            fieldData = [
+            fieldDataComplete = [
                 { name: 'Warn id / Player', value: warnedPlayers.join('\n'), inline: true },
                 { name: 'Time left', value: times.join('\n'), inline: true },
                 { name: 'Issuer / Reason', value: issuerReasonColumn.join('\n'), inline: true }
             ]
         } else {
-            fieldData = [
+            fieldDataComplete = [
                 { name: 'Warn id / Player', value: warnedPlayers.join('\n'), inline: true },
                 { name: 'Time left', value: times.join('\n'), inline: true },
                 { name: 'Reason', value: reasons.join('\n'), inline: true }
@@ -76,16 +98,45 @@ const command: Command = {
 
         const botAvatarUrl = guild.client.user.avatarURL();
 
-        const warnsCardEmbed = new EmbedBuilder()
-            .setColor('#126e82')
-            .setTitle('Warned players')
-            .addFields(fieldData)
-            .setFooter({ text: `Limited to 10 warns${warns.length > 10 ? ', one or more active warns not displayed' : ''}`, iconURL: botAvatarUrl});
+        let fieldData;
+        if (dump) {
+            fieldData = fieldDataComplete;
+            // make fieldDataComplete a nested arry of 10 elements.
+            const fieldDataNested = [];
+            for (let i = 0; i < fieldDataComplete.length; i += 10) {
+                fieldDataNested.push(fieldDataComplete.slice(i, i + 10));
+            }
+            let flushedEmbeds: EmbedBuilder[] = [];
+            for (const fd of fieldDataNested) {
+                const warnsCardEmbed = new EmbedBuilder()
+                    .setColor('#126e82')
+                    .setTitle('Warned players')
+                    .addFields(fd)
+                    .setFooter({ text: `Limited to 10 warns${warns.length > 10 ? ', one or more active warns not displayed' : ''}`, iconURL: botAvatarUrl });
+                flushedEmbeds.push(warnsCardEmbed);
+            }
 
-        if (interaction) {
-            interaction.reply({ embeds: [warnsCardEmbed] });
-        } else {
-            message.channel.send({ embeds: [warnsCardEmbed] });
+            if (interaction) {
+                interaction.reply({ embeds: flushedEmbeds });
+            } else {
+                message.channel.send({ embeds: flushedEmbeds });
+            }
+            return;
+        }
+        else {
+            fieldData = fieldDataComplete.slice(0, 10);
+            const warnsCardEmbed = new EmbedBuilder()
+                .setColor('#126e82')
+                .setTitle('Warned players')
+                .addFields(fieldData)
+                .setFooter({ text: `Limited to 10 warns${warns.length > 10 ? ', one or more active warns not displayed' : ''}`, iconURL: botAvatarUrl });
+
+            if (interaction) {
+                interaction.reply({ embeds: [warnsCardEmbed] });
+            } else {
+                message.channel.send({ embeds: [warnsCardEmbed] });
+            }
+            return;
         }
     }
 }
